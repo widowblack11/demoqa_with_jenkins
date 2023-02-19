72712d7749b36265400fe003eac4581df7bedf3f
widowblack11 <oksana.prokopenko16@gmail.com>
2023-02-19 17:45:45 +0300

    add def for attach

---
+++ demoqa_tests/utils/attach.py
@@ -0,0 +1,25 @@
+import allure
+from allure_commons.types import AttachmentType
+
+
+def add_screenshot(browser):
+    png = browser.driver.get_screenshot_as_png()
+    allure.attach(body=png, name='screenshot', attachment_type=AttachmentType.PNG, extension='.png')
+
+
+def add_logs(browser):
+    log = "".join(f'{text}\n' for text in browser.driver.get_log(log_type='browser'))
+    allure.attach(log, 'browser_logs', AttachmentType.TEXT, '.log')
+
+
+def add_html(browser):
+    html = browser.driver.page_source
+    allure.attach(html, 'page_source', AttachmentType.HTML, '.html')
+
+
+def add_video(browser):
+    video_url = "https://selenoid.autotests.cloud/video/" + browser.driver.session_id + ".mp4"
+    html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
+           + video_url \
+           + "' type='video/mp4'></video></body></html>"
+    allure.attach(html, 'video_' + browser.driver.session_id, AttachmentType.HTML, '.html')
